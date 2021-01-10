7cfa9d7705f1bf218f499399d7ee6c657b22b33f
QLiu87 <46546806+QLiu87@users.noreply.github.com>
2021-01-09 23:28:11 -0800

    added backtracking to loop through all possible candidates
    used backtracking to recursively check every possible candidates. Instead of looping through all the candidates, program exists when the target is found to save time. Code cleanup still needed.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,57 @@
 #include <iostream>
 #include <string.h>
+#include <vector>
 #include "init.h"
 using namespace std;
-
+string goal = "gseq";
+string answer;
+void pwd_recurs(vector<char>,string,int);
+int checkPwd(string);
 int main(){
-    string pwd = "zhgnnd";
-    string salt = "hfT7jp2q";
-    cout << "h = " << pwd_init(pwd, salt) << endl;
+    vector<char> alphabat;
+    for(int i = 0; i < 26; i++){
+        alphabat.push_back((char)(97 + i));
+    }
+    // for(auto a : alphabat){
+    //     cout << a <<endl;
+    // }
+    // string pwd = "zhgnnd";
+    // string salt = "hfT7jp2q";
+    // cout << "h = " << pwd_init(pwd, salt) << endl;
+    for(int i = 1; i < 7; i++){
+        pwd_recurs(alphabat, "", i);
+    }
+    //pwd_recurs(alphabat, "", 0);
+    return 0;
+}
+
+//use backtracking to get through all the possible candidates
+void pwd_recurs(vector<char> alphabat, string curr, int pwdlen){
+    if(pwdlen == 0){
+        if(checkPwd(curr)){
+            exit(0);
+        }
+        return;
+    }
+
+    // if(checkPwd(curr)){
+    //     exit(0);
+    // }
+    // if(pwdlen == 6){
+    //     return;
+    // }
+    for(int ii = 0; ii < alphabat.size(); ii++){
+        string candidate;
+        candidate = curr + alphabat[ii];
+         cout << candidate << endl;
+        pwd_recurs(alphabat, candidate, pwdlen - 1);
+    }
+}
+
+int checkPwd(string curr){
+    if(curr == goal){
+        cout << "Found:  " << curr << endl;
+        return 1;
+    }
     return 0;
 }
