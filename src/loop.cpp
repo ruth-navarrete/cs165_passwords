247b0e98c33da5db397ecac90e3004d61695dc4a
QLiu87 <46546806+QLiu87@users.noreply.github.com>
2021-01-12 17:45:27 -0800

    updated loop

--- src/loop.cpp
+++ src/loop.cpp
@@ -4,14 +4,28 @@
 using namespace std;
 
 string loop(string pwd, string salt, string h) {
+    //h.length() returns 32 here, but should be length 16. 
+    //each 2 indices of h represents a hex value, so we need to convert h
+    //to h_hex
+    //ex, h = ed7a5307588e49ed3a2777d926d62f96
+    //  ->h_hex = ed/7a/53/.../2f/96
+    //cout << "In loop, h is " << h.length() << endl;
     for(int i = 0; i < 1000; i++) {
+        string h_hex = "";
+        for(int ii = 0; ii < 32; ii += 2) {
+            unsigned char hex_placeholder;
+            string tmp = "0x";
+            tmp = tmp + h[ii] + h[ii+1];
+            hex_placeholder = stoul(tmp, nullptr, 16);
+            h_hex += hex_placeholder;
+        }
         string tmp = "";
 
         if((i % 2) != 0) {
             tmp += pwd;
         }
         else {
-            tmp += h;
+            tmp += h_hex;
         }
 
         if((i % 3) != 0) {
@@ -21,7 +35,7 @@ string loop(string pwd, string salt, string h) {
             tmp += pwd;
         }
         if((i % 2) != 0) {
-            tmp +=h;
+            tmp +=h_hex;
         }
         else {
             tmp += pwd;
@@ -29,6 +43,6 @@ string loop(string pwd, string salt, string h) {
 
         h = md5(tmp);
     }
-    
+    cout << "In loop, final h is: "<< h << endl;
     return h;
 }
